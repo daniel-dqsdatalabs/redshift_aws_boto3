3fa803f91a07b81a68c0ace8377445eaaa1ef003
Daniel <68124272+daniel-dqsdatalabs@users.noreply.github.com>
2020-09-25 21:24:07 -0300

    Update redshift_ioc.py

--- redshift_ioc.py
+++ redshift_ioc.py
@@ -1,162 +1,146 @@
-import pandas as pd
 import boto3
 import json
+import pandas as pd
 import configparser
-config = configparser.ConfigParser()
-config.read_file(open('dwh.cfg'))
 
-KEY                    = config.get('AWS','KEY')
-SECRET                 = config.get('AWS','SECRET')
+'''
+    create the aws resources needed to run project
+'''
+   
 
-DWH_CLUSTER_TYPE       = config.get("DWH","DWH_CLUSTER_TYPE")
-DWH_NUM_NODES          = config.get("DWH","DWH_NUM_NODES")
-DWH_NODE_TYPE          = config.get("DWH","DWH_NODE_TYPE")
+########################
+#       FIREWALL
+########################
 
-DWH_CLUSTER_IDENTIFIER = config.get("DWH","DWH_CLUSTER_IDENTIFIER")
-DWH_DB                 = config.get("DWH","DWH_DB")
-DWH_DB_USER            = config.get("DWH","DWH_DB_USER")
-DWH_DB_PASSWORD        = config.get("DWH","DWH_DB_PASSWORD")
-DWH_PORT               = config.get("DWH","DWH_PORT")
-
-DWH_IAM_ROLE_NAME      = config.get("DWH", "DWH_IAM_ROLE_NAME")
-
-(DWH_DB_USER, DWH_DB_PASSWORD, DWH_DB)
-
-pd.DataFrame({"Param":
-                  ["DWH_CLUSTER_TYPE", "DWH_NUM_NODES", "DWH_NODE_TYPE", "DWH_CLUSTER_IDENTIFIER", "DWH_DB", "DWH_DB_USER", "DWH_DB_PASSWORD", "DWH_PORT", "DWH_IAM_ROLE_NAME"],
-              "Value":
-                  [DWH_CLUSTER_TYPE, DWH_NUM_NODES, DWH_NODE_TYPE, DWH_CLUSTER_IDENTIFIER, DWH_DB, DWH_DB_USER, DWH_DB_PASSWORD, DWH_PORT, DWH_IAM_ROLE_NAME]
-             })
-             
-             
-             
-# creating clients for IAM, EC2, S3 and Reshift
-
-ec2 = boto3.resource('ec2',region_name="us-west-2",aws_access_key_id=KEY,aws_secret_access_key=SECRET)
-
-s3 = boto3.resource('s3',region_name="us-west-2",aws_access_key_id=KEY,aws_secret_access_key=SECRET)
-
-iam = boto3.client('iam',aws_access_key_id=KEY,aws_secret_access_key=SECRET,region_name='us-west-2')
-
-redshift = boto3.client('redshift', region_name="us-west-2", aws_access_key_id=KEY, aws_secret_access_key=SECRET)
-
-
-sampleDbBucket =  s3.Bucket("awssampledbuswest2")
-for obj in sampleDbBucket.objects.filter(Prefix="ssbgz"):
-    print(obj)
-
-
-# creating the IAM Role
-
-from botocore.exceptions import ClientError
-
-#1.1 Create the role, 
-try:
-    print("1.1 Creating a new IAM Role") 
-    dwhRole = iam.create_role(
-        Path='/',
-        RoleName=DWH_IAM_ROLE_NAME,
-        Description = "Allows Redshift clusters to call AWS services on your behalf.",
-        AssumeRolePolicyDocument=json.dumps(
-            {'Statement': [{'Action': 'sts:AssumeRole',
-               'Effect': 'Allow',
-               'Principal': {'Service': 'redshift.amazonaws.com'}}],
-             'Version': '2012-10-17'})
-    )    
-except Exception as e:
-    print(e)
-    
-    
-print("1.2 Attaching Policy")
-
-iam.attach_role_policy(RoleName=DWH_IAM_ROLE_NAME,
-                       PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-                      )['ResponseMetadata']['HTTPStatusCode']
-
-print("1.3 Get the IAM role ARN")
-roleArn = iam.get_role(RoleName=DWH_IAM_ROLE_NAME)['Role']['Arn']
-
-print(roleArn)
-
-
-# creating the redshift cluster
-
-try:
-    response = redshift.create_cluster(        
-        #HW
-        ClusterType=DWH_CLUSTER_TYPE,
-        NodeType=DWH_NODE_TYPE,
-        NumberOfNodes=int(DWH_NUM_NODES),
-
-        #Identifiers & Credentials
-        DBName=DWH_DB,
-        ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,
-        MasterUsername=DWH_DB_USER,
-        MasterUserPassword=DWH_DB_PASSWORD,
-        
-        #Roles (for s3 access)
-        IamRoles=[roleArn]  
-    )
-except Exception as e:
-    print(e)
-
-
-def prettyRedshiftProps(props):
-    pd.set_option('display.max_colwidth', -1)
-    keysToShow = ["ClusterIdentifier", "NodeType", "ClusterStatus", "MasterUsername", "DBName", "Endpoint", "NumberOfNodes", 'VpcId']
-    x = [(k, v) for k,v in props.items() if k in keysToShow]
-    return pd.DataFrame(data=x, columns=["Key", "Value"])
-
-myClusterProps = redshift.describe_clusters(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER)['Clusters'][0]
-prettyRedshiftProps(myClusterProps)
-
-
-DWH_ENDPOINT = myClusterProps['Endpoint']['Address']
-DWH_ROLE_ARN = myClusterProps['IamRoles'][0]['IamRoleArn']
-print("DWH_ENDPOINT :: ", endpoint)
-print("DWH_ROLE_ARN :: ", roleArn)
-
-# open an incoming TCP port to access the cluster endpoint
-
-try:
-    vpc = ec2.Vpc(id=myClusterProps['VpcId'])
+def open_firewall(ec2, vpc_id, config):
+    vpc = ec2.Vpc(id = vpc_id)
     defaultSg = list(vpc.security_groups.all())[0]
-    print(defaultSg)
+    
     defaultSg.authorize_ingress(
-        GroupName=defaultSg.group_name,
-        CidrIp='0.0.0.0/0',
-        IpProtocol='TCP',
-        FromPort=int(DWH_PORT),
-        ToPort=int(DWH_PORT)
+        GroupName= defaultSg.group_name, 
+        CidrIp='0.0.0.0/0',  
+        IpProtocol='TCP',  
+        FromPort=int(config.get("DWH","DB_PORT")),
+        ToPort=int(config.get("DWH","DB_PORT"))
     )
-except Exception as e:
-    print(e)
+
+########################
+#       SVC/RSC
+########################
+
+def get_instance(service, config):
+    return boto3.client(
+        service, 
+        region_name = 'sa-east-1', 
+        aws_access_key_id = config.get("AWS","KEY"), 
+        aws_secret_access_key = config.get("AWS","SECRET")
+    )   
+
+def get_rsrc_instance(resource, config):
+    return boto3.resource(
+        resource, 
+        region_name = 'sa-east-1', 
+        aws_access_key_id = config.get("AWS","KEY"), 
+        aws_secret_access_key = config.get("AWS","SECRET")
+    )   
+
+def delete_enviroment(redshift, iam, config):
+    redshift.delete_cluster(
+        ClusterIdentifier = config.get("DWH","DWH_CLUSTER_IDENTIFIER"),  
+        SkipFinalClusterSnapshot=True
+    )
     
+    iam.detach_role_policy(
+        RoleName = config.get("DWH","DWH_IAM_ROLE_NAME"),
+        PolicyArn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+    )
     
+    iam.delete_role(RoleName = config.get("DWH","DWH_IAM_ROLE_NAME"))
     
- %load_ext sql
+
+########################
+#       IAM
+########################
+
+def create_aim_role(iam, config):
+    return iam.create_role(
+        Path = '/',
+        RoleName = config.get("DWH","DWH_IAM_ROLE_NAME"), 
+        AssumeRolePolicyDocument = json.dumps({
+            'Statement': [{
+                'Action': 'sts:AssumeRole',
+                'Effect': 'Allow', 
+                'Principal': {
+                    'Service': 'redshift.amazonaws.com'
+                }
+            }
+            ],
+            'Version': '2012-10-17'
+        })
+    )
+
  
-conn_string="postgresql://{}:{}@{}:{}/{}".format(DWH_DB_USER, DWH_DB_PASSWORD, DWH_ENDPOINT, DWH_PORT,DWH_DB)
-print(conn_string)
-%sql $conn_string
+def get_role_arn(iam, config):
+    return iam.get_role(
+        RoleName = config.get("DWH","DWH_IAM_ROLE_NAME"))['Role']['Arn']
+   
+
+def attach_policy(iam, config):
+    iam.attach_role_policy(
+        RoleName = config.get("DWH","DWH_IAM_ROLE_NAME"), 
+        PolicyArn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess")['ResponseMetadata']['HTTPStatusCode']
 
 
-# cleaning the resources
+########################
+#       REDSHIFT
+########################
 
-redshift.delete_cluster( ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,  SkipFinalClusterSnapshot=True)
-
-myClusterProps = redshift.describe_clusters(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER)['Clusters'][0]
-prettyRedshiftProps(myClusterProps)
-
-iam.detach_role_policy(RoleName=DWH_IAM_ROLE_NAME, PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess")
-iam.delete_role(RoleName=DWH_IAM_ROLE_NAME)
-
-
-
-
-
-
-
-
+def create_redshift(redshift, role, config):
+    return redshift.create_cluster(        
+        ClusterType = config.get("DWH","DWH_CLUSTER_TYPE"), 
+        NodeType = config.get("DWH","DWH_NODE_TYPE"), 
+        NumberOfNodes = int(config.get("DWH","DWH_NUM_NODES")),
+        DBName = config.get("DWH","DWH_DB"),
+        ClusterIdentifier = config.get("DWH","DWH_CLUSTER_IDENTIFIER"),
+        MasterUsername = config.get("DWH","DWH_DB_USER"),
+        MasterUserPassword = config.get("DWH","DWH_DB_PASSWORD"),
+        IamRoles = role
+    )
 
 
+########################
+#       START
+########################
+
+def main():
+    '''
+        reads the config file and creates AWS services/resources
+    '''
+    try:
+        # config file
+        config = configparser.ConfigParser()
+        config.read_file(open('dwh.cfg'))
+    
+        # get services/resource clients
+        s3_cli = get_rsrc_client('s3', config)
+        ec2_cli = get_rsrc_client('ec2', config)
+        iam_cli = get_serv_client('iam', config)
+        redshift_cli = get_serv_client('redshift', config)
+        
+        #create the services
+        
+        iam_obj = create_aim_role(config)
+        attach_policy(iam_cli, config)
+        
+        role_obj = get_role_arn(iam_cli, config)
+        redshift_obj = create_redshift(redshift_cli, role_obj, config)
+        
+        vpc_id = redshift.get_prop() ##falta adicionar  #myClusterProps['VpcId'])
+        open_firewall(ec2_cli, vpc_id, config)
+        
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
